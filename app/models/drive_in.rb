80604f8948531a738f8e22f0cb98dcdd2e99bc43
Srishti Nath <srishti.nath@gmail.com>
2020-04-28 11:36:13 -0400

    final commit

--- app/models/drive_in.rb
+++ app/models/drive_in.rb
@@ -1,6 +1,7 @@
 class DriveIn
     attr_reader :name #allows return of the DriveIn name with a getter method; no setter method prevents changes to name
     @@all = []
+    
     def initialize(name)
         @name = name
         DriveIn.all << self
