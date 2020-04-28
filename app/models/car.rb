3fd0bf2fad74e31436d1ab4d0f938fb76c364ad1
carolineglass <carolineglass89@gmail.com>
2020-04-28 10:26:27 -0400

    first commit

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,33 @@
 class Car
-  attr_reader :passenger_count
-
+  attr_accessor :passenger_count
+  @@all = []
   def initialize(passenger_count)
     @passenger_count = passenger_count
+    Car.all << self
   end
+  def self.all
+    @@all
+  end
+  def current_movie_screen
+    # - Returns the current movie screen that a particular car is associated with.
+  end
+  def current_movie_screen=
+  end 
 end
+
+# #### Car
+
+# - ((`Car#initialize(passenger_count)`
+#   - A car should be initialized with a `passenger_count` (as an integer).
+#   - the passenger count **can** be changed after the `Car` is initialized))
+# - ((`Car#passenger_count`
+#   - Returns the number of passengers in the car.))
+# - ((`Car.all`
+#   - Returns an array of all car instances that have been created.))
+
+# - `Car#current_movie_screen`
+#   - Returns the current movie screen that a particular car is associated with.
+# - `Car#current_movie_screen=`
+#   - Assigns a movie screen object to a particular car to indicate which movie screen that car is currently at.
+#   - **Note:** this will happen _after_ a car has already been created.
+
--- app/models/movie_screen.rb
+++ app/models/movie_screen.rb
@@ -1,9 +1,44 @@
 class MovieScreen
   attr_reader :capacity, :movie_title, :drive_in  
-
+  @@all = []
   def initialize(movie_title, capacity, drive_in)
     @movie_title = movie_title
     @capacity = capacity
     @drive_in = drive_in
+    MovieScreen.all << self
+  end
+  def self.all 
+    @@all 
   end
 end
+
+#### MovieScreen
+
+#### MovieScreen
+
+# - ((`MovieScreen#initialize(movie_title, capacity, drive_in)`
+#   - A movie screen should be initialized with a movie title as a string, capacity (as an integer), and a `drive_in` object.
+#   - The movie title, capacity, and drive in **cannot** be changed after the MovieScreen is initialized.))
+# - ((`MovieScreen#movie_title`
+#   - should return the movie title))
+# - ((`MovieScreen#capacity`
+#   - should return the `MovieScreen`'s capacity))
+# - ((`MovieScreen#drive_in`
+#   - should return the `DriveIn` associated with this `MovieScreen`))
+# - ((`MovieScreen.all_screens`
+#   - Returns an array of all movie screens that have been created.))
+
+# - `MovieScreen#cars`
+#   - Returns an array of all cars currently at this movie screen.
+
+# - `MovieScreen#number_of_viewers`
+#   - Returns the total number of passengers viewing the movie, from all the cars currently at this movie screen
+# - `MovieScreen#at_capacity?`
+#   - Returns a boolean. If the number of cars at this movie screen is equal to or above the capacity of the movie screen, returns `true`. Otherwise, returns `false`.
+# - `MovieScreen#available_spots`
+#   - Returns the number of spots for cars available at this movie screen. This should be the capacity minus the number of cars currently at this movie screen.
+# - `MovieScreen#add_car(car)`
+#   - Takes in a `Car` instance as the argument
+#   - Depending on the available capacity of the movie screen, associates the `Car` with this movie screen.
+#     - If the movie screen is _not_ at capacity, updates the car's current movie screen and returns the string `"Enjoy!"`.
+#     - If the movie screen is at capacity, it should return the string "Sold Out!", and should not associate the car to the movie screen.
