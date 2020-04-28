c919ec0b4966cb4b43c89db7987ddab9e3fc6c3d
FCGit19 <canofred@gmail.com>
2020-04-28 11:29:50 -0400

    hi

--- app/models/drive_in.rb
+++ app/models/drive_in.rb
@@ -1,8 +1,14 @@
 class DriveIn
   attr_reader :name 
-  
+
   def initialize(name)
     @name = name 
+  end 
+
+  def screens 
+    MovieScreen.all |movie_screen|
+      movie_screen if movie_scren == self
+    end 
   end 
 
 end
@@ -14,3 +20,7 @@ end
 # The name cannot be changed after the drive-in is initialized.
 # DriveIn#name
 # should return the name of the DriveIn
+
+# DriveIn
+# DriveIn#screens
+# Returns an array of all movie screens at this drive-in.
