33cd42c1442a45479abda40fdcebd9b473b9c9df
FCGit19 <canofred@gmail.com>
2020-04-28 11:45:35 -0400

    finished

--- app/models/movie_screen.rb
+++ app/models/movie_screen.rb
@@ -16,6 +16,10 @@ class MovieScreen
     cars.self
   end 
 
+  def number_of_viewers
+
+  end 
+
 end
 
 
@@ -35,3 +39,16 @@ end
 # MovieScreen
 # MovieScreen#cars
 # Returns an array of all cars currently at this movie screen.
+
+# MovieScreen
+# MovieScreen#number_of_viewers
+# Returns the total number of passengers viewing the movie, from all the cars currently at this movie screen
+# MovieScreen#at_capacity?
+# Returns a boolean. If the number of cars at this movie screen is equal to or above the capacity of the movie screen, returns true. Otherwise, returns false.
+# MovieScreen#available_spots
+# Returns the number of spots for cars available at this movie screen. This should be the capacity minus the number of cars currently at this movie screen.
+# MovieScreen#add_car(car)
+# Takes in a Car instance as the argument
+# Depending on the available capacity of the movie screen, associates the Car with this movie screen.
+# If the movie screen is not at capacity, updates the car's current movie screen and returns the string "Enjoy!".
+# If the movie screen is at capacity, it should return the string "Sold Out!", and should not associate the car to the movie screen.
