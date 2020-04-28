fff837de58602ee20a49f7fe908f5a91a24d1c7f
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-28 10:26:29 -0400

    first commit

--- app/models/drive_in.rb
+++ app/models/drive_in.rb
@@ -1,11 +1,17 @@
 class DriveIn
 
-
+#   - The name **cannot** be changed after the drive-in is initialized.
+    attr_reader :name
 # - `DriveIn#initialize(name)`
 #   - A drive-in should be initialized with a `name` as a string.
-#   - The name **cannot** be changed after the drive-in is initialized.
+    def initialize(name)
+        @name = name
+    end
 # - `DriveIn#name`
 #   - should return the name of the `DriveIn`
+    def name
+        self.name
+    end
 
 # - `DriveIn#screens`
 #   - Returns an array of all movie screens at this drive-in.
